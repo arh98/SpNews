f7d1cbe06e6a8bbbe513f68a254195371b1309a7
Ahmadrz <a.hafizi9@outlook.com>
2022-07-03 15:31:53 +0430

    Create CategoryToNews Class

---
+++ SpNews/Models/CategoryToNews.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpNews.Models
+{
+    public class CategoryToNews
+    {
+        public int NewsId { get; set; }
+        public int CategoryId { get; set; }
+
+        //navigation property
+        public Category Category { get; set; }
+        public News News { get; set; }
+    }
+}
