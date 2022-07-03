0fd5ef25c86f51d0ecc57c135d71b64bc07909d5
Ahmadrz <a.hafizi9@outlook.com>
2022-07-03 14:38:36 +0430

    seed data to category : migration

---
+++ SpNews/Migrations/20220703100610_SeedDefaultDatatoCategoryMigration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpNews.Migrations
+{
+    public partial class SeedDefaultDatatoCategoryMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "News related to foreign sports competitions", "Foreign" },
+                    { 2, "News about domestic competitions", "Domestic" },
+                    { 3, "News about football competitions", "Football" },
+                    { 4, "News about basketball competitions", "Basketball" },
+                    { 5, "News about volleyball competitions", "Volleyball" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
