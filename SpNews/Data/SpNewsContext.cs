57202b0cfbbe0c3c69ebabdbdf19a6be6719e867
Ahmadrz <a.hafizi9@outlook.com>
2022-07-03 14:37:57 +0430

    seed some default data to Category Table

--- SpNews/Data/SpNewsContext.cs
+++ SpNews/Data/SpNewsContext.cs
@@ -16,5 +16,43 @@ namespace SpNews.Data
         }
         // models that should create table for them in db
         public DbSet<Category> Categories { get; set; }
+        //seeding some default data to db
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region seed_Data_to_CategoryTB
+            modelBuilder.Entity<Category>().HasData(
+            new Category()
+            {
+                Id = 1,
+                Name= "Foreign",
+                Description = "News related to foreign sports competitions"
+            },
+            new Category()
+            {
+                Id = 2,
+                Name = "Domestic",
+                Description = "News about domestic competitions"
+            },
+            new Category()
+            {
+                Id = 3,
+                Name = "Football",
+                Description = "News about football competitions"
+            },
+            new Category()
+            {
+                Id = 4,
+                Name = "Basketball",
+                Description = "News about basketball competitions"
+            },
+            new Category()
+            {
+                Id = 5,
+                Name = "Volleyball",
+                Description = "News about volleyball competitions"
+            });
+            #endregion
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
