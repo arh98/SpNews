bfbc60c1b7d95f08720ba837fe4c18b41a07801f
Ahmadrz <a.hafizi9@outlook.com>
2022-07-03 10:32:11 +0430

    Add project files.

---
+++ SpNews/Models/News.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpNews.Models
+{
+    public class News
+    {
+        public News()
+        {
+            Categories = new List<Category>();
+        }
+        public int  Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<Category> Categories { get; set; }
+        public string Date { get; set; }
+
+       
+    }
+}
