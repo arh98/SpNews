809e866c4215b1ddf8ba931c72ddeea01551b397
Ahmadrz <a.hafizi9@outlook.com>
2022-07-03 23:56:00 +0430

    create DetailedViewModel for news

---
+++ SpNews/Models/DetailedViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpNews.Models
+{
+    public class DetailedViewModel
+    {
+        public News News { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
