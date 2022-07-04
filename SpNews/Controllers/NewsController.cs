24f7ce5bbc4bac3e00ce79d0eea7757dafa46769
Ahmadrz <a.hafizi9@outlook.com>
2022-07-04 13:24:31 +0430

    add newsControler with ShowByGroup Action

---
+++ SpNews/Controllers/NewsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpNews.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpNews.Controllers
+{
+    public class NewsController : Controller
+    {
+        private SpNewsContext _context;
+        public NewsController(SpNewsContext context)
+        {
+            _context = context;
+        }
+        [Route("Group/{id}/{name}")]
+        public IActionResult ShowByGroup(int id , string name)
+        {
+            ViewData["GroupName"] = name;
+            var gpnews = _context.CategoryToNews
+                .Where(c => c.CategoryId == id)
+                .Include(n => n.News)
+                .Select(n => n.News)
+                .ToList();
+            return View(gpnews);
+        }
+    }
+}
