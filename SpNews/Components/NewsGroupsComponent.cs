223478cc3ba59697cf2c49dad0f5fa603e40d80b
Ahmadrz <a.hafizi9@outlook.com>
2022-07-04 13:23:20 +0430

    create NGpComponent for showing groups inside layout

---
+++ SpNews/Components/NewsGroupsComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpNews.Data;
+using SpNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpNews.Components
+{
+    public class NewsGroupsComponent : ViewComponent
+    {
+        private SpNewsContext _context;
+        public NewsGroupsComponent(SpNewsContext context)
+        {
+            _context = context;
+
+        }
+        // a method that return its view
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = _context.Categories
+                 .Select(c => new ShowGroupViewModel()
+                 {
+                     GroupId = c.Id,
+                     Name = c.Name,
+                     NewsCount = _context.CategoryToNews.Count(g => g.CategoryId == c.Id)
+                 }).ToList();
+            return View("/Views/Components/NewsGroupsComponent.cshtml", categories);
+        }
+
+    }
+}
